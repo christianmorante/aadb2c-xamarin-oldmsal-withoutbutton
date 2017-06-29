2dd0101313262da5c77ce432a22299ff42c0a71d
christian morante <christianmorante@outlook.com>
2017-06-29 11:29:06 -0500

    second commit add project files

---
+++ ADB2CAuthorization/Constants.cs
@@ -0,0 +1,11 @@
+﻿namespace ADB2CAuthorization
+{
+	public static class Constants
+	{
+        public static string ApplicationID = "75e8c18b-96dc-44dc-b255-86e3ee9ff257";
+		public static string[] Scopes = { ApplicationID };
+        public static string SignUpSignInPolicy = "B2C_1_RSA_SignUpSignInPol";
+		public static string ResetPasswordPolicy = "B2C_1_RSA_PassResetPol";
+		public static string Authority = "https://login.microsoftonline.com/agisolb2c.onmicrosoft.com/";
+	}
+}
